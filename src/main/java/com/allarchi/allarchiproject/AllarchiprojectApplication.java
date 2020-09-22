2fca47303fd68e68e82dbfed58f86279770cccf8
Valéry LO-CASCIO <valerylo-cascio@MacBook-Pro-de-Valery.local>
2020-09-22 14:52:52 +0200

    Initial commit

---
+++ src/main/java/com/allarchi/allarchiproject/AllarchiprojectApplication.java
@@ -0,0 +1,13 @@
+package com.allarchi.allarchiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllarchiprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllarchiprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/allarchi/allarchiproject/AllarchiprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allarchi.allarchiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllarchiprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
